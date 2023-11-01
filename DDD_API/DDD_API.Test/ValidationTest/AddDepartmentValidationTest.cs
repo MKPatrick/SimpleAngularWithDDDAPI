eb208dc66f710a008a7bd28d4a28ea155a032e19
Patrick Meier <104093287+MKPatrick@users.noreply.github.com>
2023-11-01 19:20:19 +0100

    create tests with fixture

---
+++ DDD_API/DDD_API.Test/ValidationTest/AddDepartmentValidationTest.cs
@@ -0,0 +1,38 @@
+﻿using AutoFixture;
+using DDD_API.DTO.Department;
+using System.ComponentModel.DataAnnotations;
+
+namespace DDD_API.Test.ValidationTest
+{
+    [TestClass]
+    public class AddDepartmentValidationTest
+    {
+        private Fixture fixture;
+
+        public AddDepartmentValidationTest()
+        {
+            fixture = new Fixture();
+
+        }
+        [TestMethod]
+        public void ShouldHaveError_WhenEverythingIsEmpty()
+        {
+            var department = fixture.Build<AddDepartmentRequest>().Without(x => x.Name).Create();
+            ValidationContext validation = new ValidationContext(department);
+            var validationResults = new List<ValidationResult>();
+            bool isValid = Validator.TryValidateObject(department, validation, validationResults, true);
+            Assert.IsFalse(isValid);
+        }
+
+
+        [TestMethod]
+        public void ShouldHaveNoError_WhenEverythingIsFilledOut()
+        {
+            var department = fixture.Build<AddDepartmentRequest>().Create();
+            ValidationContext validation = new ValidationContext(department);
+            var validationResults = new List<ValidationResult>();
+            bool isValid = Validator.TryValidateObject(department, validation, validationResults, true);
+            Assert.IsTrue(isValid);
+        }
+    }
+}
---
+++ DDD_API/DDD_API.Test/ValidationTest/AddEmployeeValidationTest.cs
@@ -0,0 +1,69 @@
+﻿using AutoFixture;
+using DDD_API.DTO.Employee;
+using DDD_API.Validation;
+using FluentValidation.TestHelper;
+
+namespace DDD_API.Test.ValidationTest
+{
+
+    [TestClass]
+    public class AddEmployeeValidationTest
+    {
+        private Fixture fixture;
+        public AddEmployeeValidationTest()
+        {
+            fixture = new Fixture();
+        }
+
+        [TestMethod]
+        public void ShouldHaveError_WhenEverythingIsEmpty()
+        {
+            var validator = new AddEmployeeValidation();
+            var model = new AddEmployeeRequest { };
+            var result = validator.TestValidate(model);
+            result.ShouldHaveAnyValidationError();
+        }
+
+        [TestMethod]
+        [DataRow("Max", "Mustermann", "Example street", "Dr.")]
+        [DataRow("P", "Meier", " street", "")]
+        public void ShouldHaveNoError(string firstname, string lastname, string address, string title)
+        {
+            var validator = new AddEmployeeValidation();
+            var model = fixture.Build<AddEmployeeRequest>().With(x => x.FirstName, firstname).With(x => x.LastName, lastname).With(x => x.Address, address).With(x => x.Title, title).Create();
+            var result = validator.TestValidate(model);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [TestMethod]
+        public void FirstName_ShouldHaveError_WhenEmpty()
+        {
+            var validator = new AddEmployeeValidation();
+            var model = fixture.Build<AddEmployeeRequest>().Without(x => x.FirstName).Create();
+            var result = validator.TestValidate(model);
+            result.ShouldHaveAnyValidationError();
+        }
+
+
+        [TestMethod]
+        public void LastName_ShouldHaveError_WhenEmpty()
+        {
+            var validator = new AddEmployeeValidation();
+            var model = fixture.Build<AddEmployeeRequest>().Without(x => x.LastName).Create();
+            var result = validator.TestValidate(model);
+            result.ShouldHaveAnyValidationError();
+        }
+
+
+        [TestMethod]
+        public void Address_ShouldHaveError_Empty()
+        {
+            var validator = new AddEmployeeValidation();
+            var model = fixture.Build<AddEmployeeRequest>().Without(x => x.Address).Create();
+            var result = validator.TestValidate(model);
+            result.ShouldHaveAnyValidationError();
+        }
+
+
+    }
+}
